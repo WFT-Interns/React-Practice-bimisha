6f4050466cafb12c068ee29af959e11f38882796
Bimisha Mishra <bimu.umib147@gmail.com>
2023-03-26 19:19:33 +0545

    feat:login page using useState Hook

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Login form using React
+The goal of this project is to create a Login form using react and its paradigms.
+****
+
+## App Features
+
+When the app is opened, first it shows the login form where username and password can be written. This projects is mainly done focusing on useState.
+![Front page Screenshot](/public/loginpageimg.png)
+****
+\
+&nbsp;
+****
+
+## To run the Project
+- Download or clone this repository
+- npm Install
+- npm start (Runs the app in the development mode)
+
---
+++ src/login.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react"
+import "./login.css";
+export const Login =() => {
+    const [userName, setuserName] = useState('');
+    const [password, setPass] = useState('');
+
+    const [message, setMessage] = useState('')
+        
+    //creating a function handleSubmit
+    const handleSubmit = (event) => {
+        event.preventDefault();//prevent from reloading the page
+        setMessage(`username: ${userName}, password: ${password}`);
+    }
+
+    const resetForm = (event) => {
+        event.preventDefault();//prevent from reloading the page
+        setMessage('');
+        setuserName('');
+        setPass('');  
+      }
+
+    return(
+        <>
+        <div className="form-container">
+            <form action="" onSubmit={handleSubmit}>
+                <label>Username:</label>
+                <input type="text" value={userName} onChange={event => setuserName(event.target.value)} name="userName"/>
+                <label>Password:</label>
+                <input type="Password" value={password} onChange={event => setPass(event.target.value)} name="password"/>
+                <div class="form-button">
+                    <button type="submit">Log In</button>
+                    <button type="clear" onClick={resetForm}>Clear</button>
+                </div>
+            </form>
+        </div>
+        <div className="display-container">
+            <h2>User Details:</h2>
+            <p>{message}</p>
+        </div>
+        </>
+    )
+}
+export default Login;
